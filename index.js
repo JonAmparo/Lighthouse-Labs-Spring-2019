637bea9d6165f292f7dd7c7ce68d3986463cdd94
Jonathan Amparo <JonathanMAmparo@gmail.com>
2019-05-05 17:08:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var navigation = {
-  x: -2,
-  y: "Banana",
-  z: "Beep",
-  speed: "raaaaid"
-};
+  var navigation = {
+      x: -2,
+      y: "Banana",
+      z: "Beep",
+      speed: "raaaaid"
+    };
 
-var ship = {
-  powerOn: false,
-  modules: [],
-  antenna: {
-    active: false
-  }
-};
+    var ship = {
+      powerOn: false,
+      modules: [],
+      antenna: {
+        active: false
+      }
+    };
 
-var radio = {
-  frequency: "Kenneth",
-  message: "Bugs are cool.",
-  beacon: false
-};
+    var radio = {
+      frequency: "Kenneth",
+      message: "Bugs are cool.",
+      beacon: false
+    };
 
 var essential;
 
@@ -56,6 +56,18 @@ function getLifeSupport() {
 }
 
 getLifeSupport();
+
+function findModuleIndex(name) {
+  for(var i = 0; i < countModules(); i++) {
+    if(availableModules[i].essential&& availableModules[i].name == name) {
+      return i;
+    } 
+  }
+}
+
+loadModule(findModuleIndex("propulsion"));
+
+
 
 
 
